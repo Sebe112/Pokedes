7eecaea5fdf5915eb558feda29bf9eee84f242b4
Sebe112 <92109053+Sebe112@users.noreply.github.com>
2025-01-23 20:37:24 +0100

    ehh

--- Menu.cs
+++ Menu.cs
@@ -2,7 +2,32 @@ namespace Pokedex {
     public class Menu{
         public static void MainMenu() {
             Console.Clear();
-            Console.WriteLine("errrm what the skibidi");
+            Console.WriteLine("Press a key (a, b, c, d, or e):");
+
+            // Read a single character without waiting for Enter
+            char input = Console.ReadKey().KeyChar;
+
+            switch (input)
+            {
+                case 'a':
+                    Console.WriteLine("You pressed 'a'.");
+                    break;
+                case 'b':
+                    Console.WriteLine("You pressed 'b'.");
+                    break;
+                case 'c':
+                    Console.WriteLine("You pressed 'c'.");
+                    break;
+                case 'd':
+                    Console.WriteLine("You pressed 'd'.");
+                    break;
+                case 'e':
+                    Console.WriteLine("You pressed 'e'.");
+                    break;
+                default:
+                    Console.WriteLine($"Invalid key '{input}'. Please press a, b, c, d, or e.");
+                    break;
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace Pokedex {
     class Program {
         static void Main(string[] args) {
+            MainMenu();
             Pokemon poke1 = new Pokemon(PokemonNavne.Caterpie, Typer.Bug, 200);
 
             Console.WriteLine(poke1.VisInfo());
