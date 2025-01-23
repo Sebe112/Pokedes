ab60348276e06ca83a475953ebe5046582d1c519
Sebe112 <sebastiankun111@gmail.com>
2025-01-23 11:05:27 +0100

    VisInfo

--- Pokemon.cs
+++ Pokemon.cs
@@ -5,12 +5,17 @@ namespace Pokedex {
         public Typer Type { get; private set; }
         public int Styrkeniveau { get; private set; }
 
-        public Pokemon(int id, PokemonNavne navn, Typer type, int styrkeniveau)
+        public Pokemon(PokemonNavne navn, Typer type, int styrkeniveau)
         {
-            ID = id;
+            ID = (int)navn;
             Navn = navn;
             Type = type;
             Styrkeniveau = styrkeniveau;
+        }
+
+        public string VisInfo()
+        {
+            return $"Pokemon ID: {ID}, Navn: {Navn}, Type: {Type}, Styrkeniveau: {Styrkeniveau}";
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿namespace Pokedex {
     class Program {
         static void Main(string[] args) {
+            Pokemon poke1 = new Pokemon(PokemonNavne.Caterpie, Typer.Bug, 200);
 
+            
         }
     }
 }
